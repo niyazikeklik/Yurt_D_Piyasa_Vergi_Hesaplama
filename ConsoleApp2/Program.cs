64c52390d9a8149062e8f3898dbbec73b369e6e6
niyazikeklik <niyazikeklik@gmail.com>
2024-08-08 12:17:09 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using ConsoleApp2;
+
+using System;
+
+HelloWorld.Init();
